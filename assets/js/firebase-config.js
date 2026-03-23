5c91d9b36930d5ecc7c1559675c1e0de399de80c
LUOX07 <roblesbarrios1416@Gmail.com>
2026-03-23 09:03:22 -0300

    Actualizar firebaseConfig con measurementId

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCDEDkrvMr5JeW0Zx5bQveAj3_LL-WNLrw",
+  apiKey: "AIzaSyCDEDkrvMr5JeW0Zx5bQveAj3_lL-WNLrw",
   authDomain: "lakajuar-7d632.firebaseapp.com",
   projectId: "lakajuar-7d632",
   storageBucket: "lakajuar-7d632.firebasestorage.app",
   messagingSenderId: "36431686822",
   appId: "1:36431686822:web:e7831e61a5b707ab9c1867",
+  measurementId: "G-6JXJ2KM0DD",
 };
 
 // Solo estos correos pueden ser administradores.
